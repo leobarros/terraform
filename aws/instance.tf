0e9fca81f9c1f2c0cea9580a11967aa5ac684ca7
Leonardo Barros <nickolback@gmail.com>
2019-12-21 11:43:16 -0300

    Adding security groups

--- aws/instance.tf
+++ aws/instance.tf
@@ -5,7 +5,34 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-00a208c7cdba991ea"
-  instance_type = "t2.micro"
+  ami             = "ami-00a208c7cdba991ea"
+  instance_type   = "t2.micro"
+  key_name        = "${aws_key_pair.my-key.key_name}"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
 }
 
+resource "aws_key_pair" "my-key" {
+  key_name   = "my-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  #ssh acess
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web_public_dns" {
+  value = "${aws_instance.web.public_dns}"
+}
