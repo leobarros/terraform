a01a65cab5388d94613c92ea513069a4e6fab4cb
Leonardo Barros <nickolback@gmail.com>
2020-01-16 07:12:45 -0300

    Creating module s3

---
+++ terraform_in_action/scripts/s3/variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+
+    description = "You have to put a name in your resource"
+
+}
+
+variable "acl" {
+  default = "private"
+}
+
+variable "versioning" {
+  default = "false"
+}
+
+variable "tags" {
+  type    = "map"
+  default = {}
+}
+
+variable "object_key" {
+  default = ""
+}
+
+variable "object_source" {
+  default = ""
+}
+
+variable "create_object" {
+  default = false
+}
+
+
+
+
