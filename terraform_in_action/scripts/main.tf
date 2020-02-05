71ade33923b097fc1a5c59862249b2e16b355fca
Leonardo Barros <nickolback@gmail.com>
2020-02-05 14:27:21 -0300

    Cloud watch examples

--- terraform_in_action/scripts/main.tf
+++ terraform_in_action/scripts/main.tf
@@ -52,3 +52,16 @@ module "bucket-2" {
 
 ## Using module S3
 
+#cloudwatch
+resource "aws_cloudwatch_metric_alarm" "monitor_ec2" {
+  alarm_name                = "alarm-ec2-80%"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+}
